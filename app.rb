9a1200256ef99830c3d419db451155a7c9d086d6
lomakin <lomakin-as@mail.ru>
2022-03-21 12:57:24 +0300

    add: new entity name Person

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :datebase, "sqlite3:my_db.db"
+
+class Person < ActiveRecord::Base 
+
+end
 
 get '/' do
-	erb "Hello! "			
+  erb "Hello! "
 end
